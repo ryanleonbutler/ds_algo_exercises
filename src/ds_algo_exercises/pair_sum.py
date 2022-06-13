4ef4c584e3b4a925c535d0114e63abcdfe882856
Ryan Butler <butryan@amazon.com>
2022-06-13 21:15:18 +0200

    pair_sum hash_map optimised

--- src/ds_algo_exercises/pair_sum.py
+++ src/ds_algo_exercises/pair_sum.py
@@ -2,7 +2,13 @@ from typing import List, Tuple
 
 
 def pair_sum(numbers: List, target_sum: int) -> Tuple:
-    for i in range(0, len(numbers)):
-        for j in range(i + 1, len(numbers)):
-            if numbers[i] + numbers[j] == target_sum:
-                return (i, j)
+    previous_nums = {}
+
+    for index, num in enumerate(numbers):
+        print(index, num)
+        complement = target_sum - num
+
+        if complement in previous_nums:
+            return (previous_nums[complement], index)
+
+        previous_nums[num] = index
